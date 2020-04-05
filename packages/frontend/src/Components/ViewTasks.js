32fa19bfd00f4036a83593f025e7f662b00da299
Sedat Can Yalçın <sedatcyalcin@gmail.com>
2020-04-05 23:48:16 +0000

    Add fetch tasks interval
    This feature implements fetching the progress every three seconds

--- packages/frontend/src/Components/ViewTasks.js
+++ packages/frontend/src/Components/ViewTasks.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -14,48 +14,83 @@ import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
     table: {
-        minWidth: 650
-    }
+        minWidth: 650,
+    },
 });
 
 function TaskList(props) {
     return (
-      <TableBody>
-          {props.tasks.map((item, index) => {
-              const endDate = item.ended_at !== null ? new Date(item.ended_at) : new Date();
-              const timePastMin = (endDate.getTime() / 60000) - (new Date(item.started_at).getTime() / 60000);
-              let percentage = "";
-              if (item.status === "rendering")
-                  percentage = ((item.renderedPixelCount/item.pixelCount)*100).toFixed(1);
-              return(
-            <TableRow key={item.id}>
-                <TableCell component="th" scope="row">
-                    {item.id}
-                </TableCell>
-                <TableCell align="right">Unknown</TableCell>
-                <TableCell align="right">{item.created_at}</TableCell>
-                <TableCell align="right">{Math.floor(timePastMin)} Min</TableCell>
-                <TableCell align="right">{item.status.toUpperCase()} {(item.status === "rendering")? `${percentage}%` : "" } </TableCell>
-                {(item.ended_at === null)?
-                  <TableCell  align="right">
-                      <Button onClick={()=>{history.push(`/task/${item.id}`)}} variant="contained" color="primary">
-                    View
-                </Button>
-                  </TableCell> : ""}
-
-            </TableRow>
-          );
-          })}
-      </TableBody>
+        <TableBody>
+            {props.tasks.map((item, index) => {
+                const endDate =
+                    item.ended_at !== null
+                        ? new Date(item.ended_at)
+                        : new Date();
+                const timePastMin =
+                    endDate.getTime() / 60000 -
+                    new Date(item.started_at).getTime() / 60000;
+                let percentage = "";
+                if (item.status === "rendering")
+                    percentage = (
+                        (item.renderedPixelCount / item.pixelCount) *
+                        100
+                    ).toFixed(1);
+                return (
+                    <TableRow key={item.id}>
+                        <TableCell component="th" scope="row">
+                            {item.id}
+                        </TableCell>
+                        <TableCell align="right">Unknown</TableCell>
+                        <TableCell align="right">{item.created_at}</TableCell>
+                        <TableCell align="right">
+                            {Math.floor(timePastMin)} Min
+                        </TableCell>
+                        <TableCell align="right">
+                            {item.status.toUpperCase()}{" "}
+                            {item.status === "rendering"
+                                ? `${percentage}%`
+                                : ""}{" "}
+                        </TableCell>
+                        {item.ended_at === null ? (
+                            <TableCell align="right">
+                                <Button
+                                    onClick={() => {
+                                        history.push(`/task/${item.id}`);
+                                    }}
+                                    variant="contained"
+                                    color="primary"
+                                >
+                                    View
+                                </Button>
+                            </TableCell>
+                        ) : (
+                            ""
+                        )}
+                    </TableRow>
+                );
+            })}
+        </TableBody>
     );
 }
 
 export default function ViewTasks() {
     const classes = useStyles();
     const [tasks, setTasks] = useState([]);
+    const [err, setErr] = useState("");
+    useEffect(() => {
+        axios // send a request when component is mounted
+            .get(CONFIG.serverUrl + "/scene")
+            .then((res) => setTasks(res.data.results))
+            .catch((err) => setErr(err));
+        // request every 3 seconds after component is mounted
+        const fetchTasksInterval = setInterval(() => {
+            axios
+                .get(CONFIG.serverUrl + "/scene")
+                .then((res) => setTasks(res.data.results))
+                .catch((err) => setErr(err));
+        }, 3000);
 
-    useEffect(()=>{
-        axios.get(CONFIG.serverUrl + "/scene").then(res => setTasks(res.data.results));
+        return () => clearInterval(fetchTasksInterval);
     }, []);
     return (
         <TableContainer component={Paper} style={{ marginTop: "5%" }}>
@@ -69,7 +104,8 @@ export default function ViewTasks() {
                         <TableCell align="right">Status</TableCell>
                     </TableRow>
                 </TableHead>
-                <TaskList tasks={tasks}/>
+                <TaskList tasks={tasks} />
+                {err && <span>{err.message}</span>}
             </Table>
         </TableContainer>
     );
