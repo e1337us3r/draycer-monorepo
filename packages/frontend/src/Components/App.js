2c49bfee05f40c60a5416a323043ed39d72b400b
sedatcyalcin <sedatcyalcin@gmail.com>
2020-05-09 23:19:44 +0300

    Move login/register components

--- packages/frontend/src/Components/App.js
+++ packages/frontend/src/Components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, withRouter } from "react-router-dom";
 import history from "./history";
-import Login from "./auth/Login";
-import Register from "./auth/Register";
+import Login from "./Login";
+import Register from "./Register";
 import Editor from "./EditorUI";
 import ConsoleLanding from "./ConsoleLanding";
 import Landing from "./Landing";
@@ -12,42 +12,42 @@ import Services from "./Services";
 import ViewTask from "./ViewTask";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Landing} />
-                <Route path="/login" exact>
-                    <NavBar />
-                    <Login />
-                </Route>
-                <Route path="/register">
-                    <NavBar />
-                    <Register />
-                </Route>
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/login" exact>
+          <NavBar />
+          <Login />
+        </Route>
+        <Route path="/register">
+          <NavBar />
+          <Register />
+        </Route>
 
-                <Route path="/console">
-                    <NavBar />
-                    <ConsoleLanding />
-                </Route>
-                <Route path="/editor">
-                    <NavBar />
-                    <Editor />
-                </Route>
-                <Route path="/tasks">
-                    <NavBar />
-                    <ViewTasks />
-                </Route>
-                <Route path="/services">
-                    <NavBar />
-                    <Services />
-                </Route>
-                <Route path="/task/:id">
-                    <NavBar />
-                    <ViewTask />
-                </Route>
-            </Switch>
-        </BrowserRouter>
-    );
+        <Route path="/console">
+          <NavBar />
+          <ConsoleLanding />
+        </Route>
+        <Route path="/editor">
+          <NavBar />
+          <Editor />
+        </Route>
+        <Route path="/tasks">
+          <NavBar />
+          <ViewTasks />
+        </Route>
+        <Route path="/services">
+          <NavBar />
+          <Services />
+        </Route>
+        <Route path="/task/:id">
+          <NavBar />
+          <ViewTask />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
