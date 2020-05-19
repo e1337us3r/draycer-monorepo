a7a0f421d36cdf7d737907a8df8eac596579348c
sedatcyalcin <sedatcyalcin@gmail.com>
2020-05-19 23:38:47 +0300

    Refactor for chrome

--- packages/frontend/src/Components/ConsoleLanding.js
+++ packages/frontend/src/Components/ConsoleLanding.js
@@ -12,50 +12,38 @@ const LandingPage = () => {
       }}
     >
       <Link to="/editor">
-        <Tooltip title="Create a new scene" arrow>
-          <Button
-            style={{ width: "100%" }}
-            variant="contained"
-            color="secondary"
-          >
-            New Task
-          </Button>
-        </Tooltip>
+        <Button style={{ width: "100%" }} variant="contained" color="secondary">
+          New Task
+        </Button>
       </Link>
       <br />
       <Link to="/tasks">
-        <Tooltip title="View your render tasks" arrow>
-          <Button
-            style={{ width: "100%", marginTop: "10px" }}
-            variant="contained"
-            color="secondary"
-          >
-            View Tasks
-          </Button>
-        </Tooltip>
+        <Button
+          style={{ width: "100%", marginTop: "10px" }}
+          variant="contained"
+          color="secondary"
+        >
+          View Tasks
+        </Button>
       </Link>
       <br />
       <Link to="/services">
-        <Tooltip title="Become a worker!" arrow>
-          <Button
-            style={{ width: "100%", marginTop: "10px" }}
-            variant="contained"
-            color="secondary"
-          >
-            My Services
-          </Button>
-        </Tooltip>
+        <Button
+          style={{ width: "100%", marginTop: "10px" }}
+          variant="contained"
+          color="secondary"
+        >
+          My Services
+        </Button>
       </Link>
       <Link to="/work_record">
-        <Tooltip title="View your worker history" arrow>
-          <Button
-            style={{ width: "100%", marginTop: "10px" }}
-            variant="contained"
-            color="secondary"
-          >
-            Work Record
-          </Button>
-        </Tooltip>
+        <Button
+          style={{ width: "100%", marginTop: "10px" }}
+          variant="contained"
+          color="secondary"
+        >
+          Work Record
+        </Button>
       </Link>
     </div>
   );
